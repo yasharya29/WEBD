6accd912b780319e734c89dbd563c4714f6fad5e
Yash <yash.ace21@sot.pdpu.ac.in>
2023-06-01 17:56:23 +0530

    w3 schools almost done

--- jqueryy.js
+++ jqueryy.js
@@ -11,7 +11,9 @@ $(document).ready(function () {
 
         {
             click: function () {
-                $('#paragraph').toggle();
+                $('#paragraph').toggle(1000, function () {
+                    alert("Callback function called,if you don't write function in second argument then it will execute this first")
+                });
             }
 
         }
@@ -38,17 +40,54 @@ $(document).ready(function () {
 
         }
     )
-    var ogw = $('#parahraph').width;
-    var ogh = $('#parahraph').height;
+
 
     $('#but3').on(
         {
             click: function () {
-                $('#paragraph').animate({
-
-
-                }, "fast");
+                $('#paragraph').fadeToggle("fast");
             }
         }
     )
+    $('#but4').on(
+        {
+            click: function () {
+                $('#paragraph').fadeToggle(0).css("font-family", "Papyrus").fadeToggle(1000);
+            }
+        }
+    )
+    $("#but5").click(function () {
+        alert("Text: " + $("#paragraph").text());
+    });
+
+    $("#but6").click(function () {
+        alert("HTML: " + $("#paragraph").html());
+    });
+    $("#but7").click(function () {
+        alert("Value: " + $("#fname").val());
+    });
+
+
+    $(document).ready(function () {
+        $("input").focus(function () {
+            $(this).css("background-color", "yellow");
+        });
+        $("input").blur(function () {
+            $(this).css("background-color", "green");
+        });
+    });
+    $("p").on({
+        mouseenter: function () {
+            $(this).css("background-color", "white");
+        },
+        mouseleave: function () {
+            $(this).css("background-color", "white");
+        },
+        click: function () {
+            $(this).css("background-color", "yellow");
+        },
+        dblclick: function () {
+            $(this).css("background-color", "red");
+        }
+    });
 });
