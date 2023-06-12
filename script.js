57538545f572943f7cd96013a4172839d2f4a490
Yash <yash.ace21@sot.pdpu.ac.in>
2023-06-12 18:43:47 +0530

    first day done

--- script.js
+++ script.js
@@ -3,4 +3,31 @@ $(document).ready(function () {
         $(this).toggleClass('open');
         $("#menu").toggle(400);
     });
+
+    $(".scroll-content").slick({
+
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 });
