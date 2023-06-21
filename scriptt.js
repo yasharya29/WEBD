2285f4dfa81d2489e54643a8596c439855175de1
Yash <yash.ace21@sot.pdpu.ac.in>
2023-06-21 15:10:32 +0530

    responsive

--- scriptt.js
+++ scriptt.js
@@ -3,4 +3,47 @@ $(document).ready(function () {
 
         $(".burger").toggle(100);
     });
+
+
+
+    $(window).scroll(function () {
+        // Get the scroll position
+        var scrollPosition = $(window).scrollTop();
+
+        // Show elements based on scroll position
+        $(".intro").each(function () {
+            var elementOffset = $(this).offset().top;
+            if (scrollPosition > elementOffset - $(window).height() + 200) {
+                $(this).addClass("show");
+            }
+        });
+
+        $(".cardcontainer .card").each(function () {
+            var elementOffset = $(this).offset().top;
+            if (scrollPosition > elementOffset - $(window).height() + 200) {
+                $(this).addClass("show");
+            }
+        });
+
+        $(".info").each(function () {
+            var elementOffset = $(this).offset().top;
+            if (scrollPosition > elementOffset - $(window).height() + 200) {
+                $(this).addClass("show");
+            }
+        });
+
+        $(".aboutus").each(function () {
+            var elementOffset = $(this).offset().top;
+            if (scrollPosition > elementOffset - $(window).height() + 200) {
+                $(this).addClass("show");
+            }
+        });
+
+        $(".ourmission").each(function () {
+            var elementOffset = $(this).offset().top;
+            if (scrollPosition > elementOffset - $(window).height() + 200) {
+                $(this).addClass("show");
+            }
+        });
+    });
 });
