02c8033b66ba8d17ad92017400f2d70240c949d1
Yash <yash.ace21@sot.pdpu.ac.in>
2023-06-26 18:26:50 +0530

    layout complete

--- MinimalMom/script.js
+++ MinimalMom/script.js
@@ -2,35 +2,55 @@
 $(document).ready(function () {
 
     $('.slider-wrapper').slick({
-        dots: true,
+        /* dots: true, */
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        /*         responsive: [
-                    {
-                        breakpoint: 1024,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1,
-                            infinite: true,
-                            dots: true
-                        }
-                    },
-                    {
-                        breakpoint: 600,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1
-                        }
-                    },
-                    {
-                        breakpoint: 480,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1
-                        }
-                    }
-                ] */
     });
+    $(window).scroll(function () {
+        var navbar = $('#logo');
+        var navLinks = $('.nav-link');
+
+        if ($(window).scrollTop() > 100) {
+            setTimeout(function () {
+                navbar.addClass('expanded');
+            });
+        } else {
+            setTimeout(function () {
+                navbar.removeClass('expanded');
+            });
+        }
+    });
+    $(window).scroll(function () {
+        var navbar = $('.navbar');
+        var navLinks = $('.nav-link');
+
+        if ($(window).scrollTop() > 100) {
+            setTimeout(function () {
+                navbar.addClass('expanded');
+            });
+        } else {
+            setTimeout(function () {
+                navbar.removeClass('expanded');
+            });
+        }
+    });
+    var btn = $('#buttton');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, '300');
+    });
+
+
+
 });
